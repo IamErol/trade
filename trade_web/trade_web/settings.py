580fc3ade4ab478a97a22f60498b8ae4c0a9801e
erolyildirim <erol.yildirim@uzinfocom.uz>
2025-12-10 23:18:43 +0500

    deployment

--- trade_web/trade_web/settings.py
+++ trade_web/trade_web/settings.py
@@ -117,10 +117,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
